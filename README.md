be1d259f2d71e4a620ca8a7d16ad80b9ebe00b14
Christian Dürr <contact@christianduerr.com>
2017-05-28 01:09:52 +0200

    Update dotfiles to new management style
    The old way of managing dotfiles was too complicated. Streamlined a lot
    of shit. Back to the basics.

--- README.md
+++ README.md
@@ -1,25 +1,17 @@
 # dotfiles
 My Linux config files.
 
-Using dotfiler (https://gitlab.com/chrisduerr/dotfiler) to automatically manage dotfiles.
-
 Dotfiles for the following software:
  - i3-gaps
  - compton
- - neofetch-git
  - neovim
- - gtk3-theme
- - pcmanfm
- - some scripts
+ - scripts
  - xinit
  - xresources
- - numix-icon-theme-git (+numix-folders)
- - oh-my-zsh-git (+powerlevel9k)
- - rofi-git
+ - oh-my-zsh
  - termite
+ - lemonbar
+ - ssh
 
 Screenshot:
-![alt tag](http://i.imgur.com/8CA77ca.png)
-=======
-# bar-helpers
-A few different tools written in Rust used for supplementing lemonbar.
+![Screenshot](http:/imgur.com/8CA77ca.png)
---
+++ install.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+function dependency {
+    pacaur -Qi $1 &> /dev/null && return
+    read -p "Depenencies required: Do you want to install '$1'? [y/N] " choice
+    if [[ "${choice^^}" == Y ]]; then
+        pacaur -S $1
+    fi
+}
+
+function symlink {
+    CURRENT_REALPATH=$(realpath -m "$2")
+    TARGET_REALPATH=$(realpath -m "$1")
+    if [[ "$CURRENT_REALPATH" != "$TARGET_REALPATH" ]]; then
+        if [[ ! -f "$2" ]] || [[ ! -d "$2" ]]; then
+            rm -rf "$2"
+        fi
+        ln -sr $1 $2
+    fi
+}
+
+# Compton
+echo "Installing compton…"
+dependency "compton"
+symlink ./files/compton.conf ~/.config/compton.conf
+
+# Neovim
+echo "Installing neovim…"
+dependency "python python-neovim neovim"
+symlink ./files/init.vim ~/.config/nvim/init.vim
+
+# Scripts
+echo "Installing scripts…"
+symlink ./files/Scripts ~/Scripts
+
+# Termite
+echo "Installing termite…"
+dependency "termite otf-fira-mono ttf-font-awesome"
+symlink ./files/termiteconfig ~/.config/termite/config
+
+# Xresources
+echo "Installing xresources…"
+dependency "otf-fira-mono"
+symlink ./files/Xresources ~/.Xresources
+
+# i3-gaps
+echo "Installing i3…"
+dependency "i3-gaps-git otf-fira-mono ttf-font-awesome maim slop compton feh lemonbar-xft-git"
+symlink ./files/i3config ~/.config/i3/config
+
+# UndeadLemon
+echo "Installing UndeadLemon…"
+dependency "lemonbar-xft-git"
+symlink ./files/lemonconfig.toml ~/.config/undeadlemon/config.toml
+
+# SSH
+echo "Installing ssh…"
+dependency "openssh"
+symlink ./files/sshconfig ~/.ssh/config
+
+# Xinitrc
+echo "Installing xinit…"
+dependency "xorg-xinit"
+symlink ./files/xinitrc ~/.xinitrc
+
+# Zsh
+echo "Installing zsh…"
+dependency "oh-my-zsh-git zsh-autosuggestions"
+symlink ./files/zshrc ~/.zshrc
