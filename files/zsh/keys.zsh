c9da5c5e747b145111008a313ae1efd22460c448
Christian Dürr <contact@christianduerr.com>
2017-08-10 20:26:54 +0200

    Add del keybind to zsh

--- files/zsh/keys.zsh
+++ files/zsh/keys.zsh
@@ -3,3 +3,5 @@ bindkey '\e[B' down-line-or-beginning-search                # [Down]
 
 bindkey "^[[1;5C" forward-word                              # [Ctrl-Right]
 bindkey '^[[1;5D' backward-word                             # [Ctrl-Left]
+
+bindkey "${terminfo[kdch1]}" delete-char                    # [Del]
