8eb20698eacee43cb803777340e8cd055869a29b
Christian Duerr <contact@christianduerr.com>
2018-08-04 16:45:10 +0200

    Update utility scripts for pasting/recording

--- files/scripts/screenrecord.sh
+++ files/scripts/screenrecord.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
-RUNNING=$(ps -ax | grep ffmpeg | wc -l)
 
-if [[ $RUNNING != 1 ]]; then
+# Video bitrate used for both recording and transcoding
+output="/home/undeadleech/pictures/tmp"
+framerate="60"
+bitrate="10M"
+
+# If recording is in process, cancel it to start transcoding
+running=$(ps ax | grep ffmpeg | wc -l)
+if [[ $running != 1 ]]; then
     pkill ffmpeg;
     exit 0;
 fi
 
-SLOP=$(slop)
-RECTANGLE=$(echo $SLOP | sed 's/\([^+]*\).*/\1/')
-OFFSET=$(echo $SLOP | sed 's/[^+]*+\(.*\)/\1/')
-XOFFSET=$(echo $OFFSET | sed 's/\([^+]*\).*/\1/')
-YOFFSET=$(echo $OFFSET | sed 's/[^+]*+\(.*\)/\1/')
-echo "Recording $RECTANGLE+$OFFSET"
-ffmpeg -y -f x11grab -s $RECTANGLE -framerate 60 -i :0.0+$XOFFSET,$YOFFSET ~/pictures/tmp.mkv
-ffmpeg -y -i ~/pictures/tmp.mkv ~/pictures/tmp.webm
+# Get the area for recording from slop
+slop=$(slop)
+rectangle=$(echo $slop | sed 's/\([^+]*\).*/\1/')
+offset=$(echo $slop | sed 's/[^+]*+\(.*\)/\1/')
+xoffset=$(echo $offset | sed 's/\([^+]*\).*/\1/')
+yoffset=$(echo $offset | sed 's/[^+]*+\(.*\)/\1/')
+
+# Record the video as mkv and transcode it to webm once recording is finished
+ffmpeg -y -f x11grab -s $rectangle -framerate $framerate -i :0.0+$xoffset,$yoffset -b:v $bitrate $output.mkv
+ffmpeg -y -i $output.mkv -b:v $bitrate $output.webm
