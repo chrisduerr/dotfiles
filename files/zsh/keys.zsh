d479fe7858511c109e8b7d9c7d5c6405ba452707
Christian Duerr <contact@christianduerr.com>
2019-03-25 17:51:30 +0100

    Add ssh alias and change zsh autosuggest colors
    This makes sure ssh is always started with the `xterm-256color` terminfo
    so the remote does not need the `alacritty` terminfo.
    
    To make the autosuggest for zsh a little more readable, its colors are
    also changed.

--- files/zsh/keys.zsh
+++ files/zsh/keys.zsh
@@ -1,7 +1,7 @@
 bindkey '^N' up-line-or-beginning-search                    # [Ctrl-N]
 bindkey '^T' down-line-or-beginning-search                  # [Ctrl-T]
 
-bindkey "^S" forward-word                                   # [Ctrl-Right] FIXME
+bindkey "^S" forward-word                                   # [Ctrl-Right]
 bindkey "^H" backward-word                                  # [Ctrl-Left]
 
 bindkey "${terminfo[kdch1]}" delete-char                    # [Del]
