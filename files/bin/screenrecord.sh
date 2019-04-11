adc3d8df3b0aa34fae2c80b15d29f3739682d005
Christian Duerr <contact@christianduerr.com>
2019-04-11 20:47:04 +0200

    Fix ffmpeg screen recording config

--- files/bin/screenrecord.sh
+++ files/bin/screenrecord.sh
@@ -2,8 +2,6 @@
 
 # Video bitrate used for both recording and transcoding
 output="/home/undeadleech/pictures/tmp"
-framerate="60"
-bitrate="10M"
 
 # If recording is in process, cancel it to start transcoding
 running=$(ps ax | grep ffmpeg | wc -l)
@@ -14,11 +12,18 @@ fi
 
 # Get the area for recording from slop
 slop=$(slop)
-rectangle=$(echo $slop | sed 's/\([^+]*\).*/\1/')
-offset=$(echo $slop | sed 's/[^+]*+\(.*\)/\1/')
-xoffset=$(echo $offset | sed 's/\([^+]*\).*/\1/')
-yoffset=$(echo $offset | sed 's/[^+]*+\(.*\)/\1/')
+width=$(echo $slop | sed 's/\([^x]*\).*/\1/')
+height=$(echo $slop | sed 's/[^x]*x\([^+]*\).*/\1/')
+xoffset=$(echo $slop | sed 's/[^+]*+\([^+]*\).*/\1/')
+yoffset=$(echo $slop | sed 's/.*+\(.*\)/\1/')
 
-# Record the video as mkv and transcode it to webm once recording is finished
-ffmpeg -y -f x11grab -s $rectangle -framerate $framerate -i :0.0+$xoffset,$yoffset -b:v $bitrate $output.mkv
-ffmpeg -y -i $output.mkv -b:v $bitrate $output.webm
+# Make sure width and height are divisible by 2
+if [[ $(($width % 2)) != 0 ]]; then
+    width=$(($width - 1))
+fi
+if [[ $(($height % 2)) != 0 ]]; then
+    height=$(($height - 1))
+fi
+
+# Record the video as mp4
+ffmpeg -y -f x11grab -s ${width}x${height} -re -i :0.0+$xoffset,$yoffset -crf 30 -c:v libx264 -pix_fmt yuv420p -preset fast -an $output.mp4
