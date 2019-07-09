e812bf72eeebe68e8ecb05a05a7ee7c7cd730c0c
Christian Duerr <contact@christianduerr.com>
2019-07-10 01:44:01 +0200

    Fix zsh vimode bindings

--- files/zsh/keys.zsh
+++ files/zsh/keys.zsh
@@ -2,9 +2,15 @@
 export KEYTIMEOUT=1
 bindkey -v
 
-# Fish autosuggestions
-bindkey '^K' up-line-or-beginning-search
-bindkey '^J' down-line-or-beginning-search
+# Fix backspace after leaving vi mode
+bindkey '^?' backward-delete-char
 
-# Delet key
+# Fix delete last word after leaving vi mode
+bindkey '^w' backward-kill-word
+
+# Fish autosuggestions
+bindkey '^N' up-line-or-beginning-search
+bindkey '^P' down-line-or-beginning-search
+
+# DEL key
 bindkey "${terminfo[kdch1]}" delete-char
