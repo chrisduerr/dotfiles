7de7fe2a3e26f26dc5bb8c146dc2a78dbe28f702
Christian Duerr <contact@christianduerr.com>
2019-07-12 02:32:11 +0200

    Fix vim word deletion

--- files/zsh/keys.zsh
+++ files/zsh/keys.zsh
@@ -1,3 +1,10 @@
+# DEL key
+bindkey "${terminfo[kdch1]}" delete-char
+
+# Fish autosuggestions
+bindkey '^N' up-line-or-beginning-search
+bindkey '^P' down-line-or-beginning-search
+
 # Vi Mode
 export KEYTIMEOUT=1
 bindkey -v
@@ -6,11 +13,33 @@ bindkey -v
 bindkey '^?' backward-delete-char
 
 # Fix delete last word after leaving vi mode
-bindkey '^w' backward-kill-word
+fix() {
+	CURSOR=0
+	zle .vi-insert
+	CURSOR=$1
+}
+vi-insert() { fix $CURSOR }
+vi-add-next() { fix $(($CURSOR + 1)) }
+vi-add-eol() { fix $#BUFFER }
+zle -N vi-insert
+zle -N vi-add-next
+zle -N vi-add-eol
 
-# Fish autosuggestions
-bindkey '^N' up-line-or-beginning-search
-bindkey '^P' down-line-or-beginning-search
+# Allow replacing inside of parenthesis/quotes
+autoload -U select-quoted select-bracketed surround
+zle -N select-quoted
+zle -N select-bracketed
+zle -N delete-surround surround
+zle -N change-surround surround
 
-# DEL key
-bindkey "${terminfo[kdch1]}" delete-char
+for m in visual viopp; do
+	for c in {a,i}{\',\",\`}; do
+		bindkey -M $m $c select-quoted
+	done
+	for c in {a,i}${(s..)^:-'()[]{}<>bB'}; do
+		bindkey -M $m $c select-bracketed
+	done
+done
+
+bindkey -a cs change-surround
+bindkey -a ds delete-surround
