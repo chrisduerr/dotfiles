9ec8a1019a0a49c08064a8c75b6a62a58af2a072
Christian Duerr <contact@christianduerr.com>
2019-12-21 01:49:25 +0100

    Disable mce for 3960x

--- files/bin/efibootmgr.sh
+++ files/bin/efibootmgr.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-efibootmgr --timeout 0 -d /dev/sda -p 1 -c -L "Arch Linux" -l /vmlinuz-linux -u "root=/dev/sda2 rw initrd=/intel-ucode.img initrd=/initramfs-linux.img quiet loglevel=0 libahci.ignore_sss=1 amdgpu.cik_support=1 radeon.cix_support=0"
+efibootmgr --timeout 0 -d /dev/sda -p 1 -c -L "Arch Linux" -l /vmlinuz-linux -u "root=/dev/sda2 rw initrd=/intel-ucode.img initrd=/initramfs-linux.img quiet loglevel=0 libahci.ignore_sss=1 amdgpu.cik_support=1 radeon.cik_support=0 mce=off"
