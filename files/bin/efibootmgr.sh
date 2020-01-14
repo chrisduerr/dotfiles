4837ef4474f6a66097e2d9d20fa9656703ff1c1e
Christian Duerr <contact@christianduerr.com>
2020-01-15 00:12:27 +0100

    Add init system to bootloader entry

--- files/bin/efibootmgr.sh
+++ files/bin/efibootmgr.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-efibootmgr --timeout 0 -d /dev/sda -p 1 -c -L "Arch Linux" -l /vmlinuz-linux -u "root=/dev/sda2 rw initrd=/intel-ucode.img initrd=/initramfs-linux.img quiet loglevel=0 libahci.ignore_sss=1 amdgpu.cik_support=1 radeon.cik_support=0 mce=off"
+efibootmgr --timeout 0 -d /dev/sda -p 1 -c -L "Arch Linux" -l /vmlinuz-linux -u "root=/dev/sda2 rw initrd=/initramfs-linux.img init=/usr/lib/systemd/systemd quiet libahci.ignore_sss=1 amdgpu.cik_support=1 radeon.cik_support=0 mce=off"
