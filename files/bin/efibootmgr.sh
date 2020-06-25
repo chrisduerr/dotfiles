9de32905bdbf09f377b369eec57fd26e3698fd2f
Christian Duerr <contact@christianduerr.com>
2020-06-25 17:15:31 +0200

    Fix efibootmgr backslashes

--- files/bin/efibootmgr.sh
+++ files/bin/efibootmgr.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-efibootmgr --timeout 0 -d /dev/disk/by-id/nvme-eui.0025385c9150334e -p 2 -c -L "Arch Linux" -l /vmlinuz-linux -u "root=/dev/md/root rw initrd=/amd-ucode.img initrd=/initramfs-linux.img init=/usr/lib/systemd/systemd quiet libahci.ignore_sss=1 amdgpu.cik_support=1 radeon.cik_support=0"
+efibootmgr --timeout 0 -d /dev/disk/by-id/nvme-eui.0025385c9150334e -p 2 -c -L "Arch Linux" -l /vmlinuz-linux -u "root=/dev/md/root rw initrd=\amd-ucode.img initrd=\initramfs-linux.img init=/usr/lib/systemd/systemd quiet libahci.ignore_sss=1 amdgpu.cik_support=1 radeon.cik_support=0"
