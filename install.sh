d653da93c4e06039ec6e231b2fee2f0df8a2fc2a
Christian Duerr <contact@christianduerr.com>
2020-06-25 16:51:20 +0200

    Fix install formatting issues

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ dependency "picom"
 mkdir -p ~/.config/picom
 ln --force ./files/picom.conf ~/.config/picom/picom.conf
 
-echo "Installing neovim…"
+echo "Installing NeoVim…"
 dependency "python python-neovim neovim"
 mkdir -p ~/.config/nvim/.backup
 ln --force ./files/init.vim ~/.config/nvim/init.vim
@@ -24,42 +24,42 @@ ln --force ./files/init.vim ~/.config/nvim/init.vim
 echo "Installing binaries…"
 ln -sr --force ./files/bin ~/
 
-echo "Installing alacritty…"
-dependency "alacritty-git otf-fira-mono ttf-font-awesome"
-mkdir -p ~/.config/alacritty
-ln --force ./files/alacritty.yml ~/.config/alacritty/alacritty.yml
-
 echo "Installing i3…"
-dependency "i3-gaps otf-fira-mono ttf-font-awesome maim slop compton feh"
+dependency "i3-gaps"
 mkdir -p ~/.config/i3
 ln --force ./files/i3config ~/.config/i3/config
 
-echo "Installing ssh…"
+echo "Installing SSH…"
 dependency "openssh"
 mkdir -p ~/.ssh
 ln --force ./files/sshconfig ~/.ssh/config
 
-echo "Installing xinit…"
+echo "Installing X…"
 dependency "xorg-xinit xorg-server"
 ln --force ./files/xinitrc ~/.xinitrc
 
-echo "Installing zsh…"
+echo "Installing Zsh…"
 dependency "zsh zsh-autosuggestions"
 ln --force ./files/zshrc ~/.zshrc
 ln --force ./files/zprofile ~/.zprofile
 ln -sr --force ./files/zsh ~/.config/
 
-echo "Installing weechat…"
-dependency "weechat-python3-git"
+echo "Installing WeeChat…"
+dependency "weechat"
 mkdir -p ~/.weechat
 ln --force ./files/weechat/weechat.conf ~/.weechat/weechat.conf
 ln --force ./files/weechat/buflist.conf ~/.weechat/buflist.conf
 ln --force ./files/weechat/irc.conf ~/.weechat/irc.conf
 ln -sr --force ./files/weechat/python ~/.weechat/
 
+echo "Setting up Alacritty…"
+dependency "ttf-fira-mono"
+mkdir -p ~/.config/alacritty
+ln --force ./files/alacritty.yml ~/.config/alacritty/alacritty.yml
+
 echo "Setting up XDG directory names…"
 ln --force ./files/xdg.dirs ~/.config/user-dirs.dirs
 
-echo "Installing GTK theme…"
+echo "Setting up GTK theme…"
 mkdir -p ~/.config/gtk-3.0
 ln --force ./files/gtk3-settings.ini ~/.config/gtk-3.0/settings.ini
