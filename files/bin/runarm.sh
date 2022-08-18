790b8d5408e28fc6be64e8d45ee49a4c90be9dcf
Christian Duerr <contact@christianduerr.com>
2022-08-18 06:49:56 +0200

    Add pinephone remote compile scripts

---
+++ files/bin/runarm.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+
+# Build project for aarch64.
+
+if [ $# -lt 1 ]; then
+    echo "USAGE: runarm.sh [USER@]<HOST>"
+    exit 1
+fi
+
+# Target platform sysroot.
+SYSROOT=$(realpath ~/programming/linux/alarm-sysroot)
+
+# Setup pkgconfig.
+export PKG_CONFIG_LIBDIR="${SYSROOT}/usr/lib/pkgconfig:${SYSROOT}/usr/share/pkgconfig"
+export PKG_CONFIG_SYSROOT_DIR="${SYSROOT}"
+export PKG_CONFIG_ALLOW_CROSS=true
+
+# Setup linker.
+export RUSTFLAGS="-C linker=aarch64-linux-gnu-gcc -C link-arg=--sysroot=${SYSROOT}"
+
+# Build!
+cargo build --release --target=aarch64-unknown-linux-gnu
+
+
+# Run project on a separate device through SSH.
+
+remote="$1"
+
+# Exit on error
+set -e
+
+# Get executable
+binpath=$(find target/aarch64-unknown-linux-gnu/release -maxdepth 1 -executable -type f | head -n 1)
+bin=$(basename "$binpath")
+
+# Strip to improve copy speed
+aarch64-linux-gnu-strip "$binpath"
+
+# Kill potentially running previous executables
+ssh "$remote" "pkill $bin; rm $bin" 2> /dev/null || true
+
+# Copy to the remote machine
+scp "$binpath" "${remote}:~"
+
+# Execute on TTY1
+if [ "$bin" == "catacomb" ]; then
+    ssh "$remote" "setsid sh -c './$bin <> /dev/tty1'"
+else
+    ssh "$remote" "WAYLAND_DISPLAY=wayland-0 ./$bin"
+fi
