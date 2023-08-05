45608a355441938cd70684fcd46990ff285d147a
Christian Duerr <contact@christianduerr.com>
2023-08-05 20:38:26 +0200

    Add Catacomb scale override for Firefox

--- files/catacomb.sh
+++ files/catacomb.sh
@@ -6,6 +6,9 @@ set -e
 # Key Bindings
 catacomb msg bind-key '*' XF86PowerOff tzompantli
 
+# Per-application scale overrides
+catacomb msg scale --app-id firefox 1.5
+
 # Spawn Shit
 swaybg -i ~/pictures/wallpapers/skeleton.jpg --mode center &
 squeekboard &
