58f6014298e1f93197bdd52e94a8a83d8d7903d3
Christian Duerr <contact@christianduerr.com>
2023-12-21 19:02:28 +0100

    Update catacomb config

--- files/catacomb.sh
+++ files/catacomb.sh
@@ -6,8 +6,12 @@ set -e
 # Key Bindings
 catacomb msg bind-key '*' XF86PowerOff tzompantli
 
+# Gesture key bindings
+catacomb msg bind-gesture-key 'Alacritty' tr mr --mods ctrl,shift v
+
 # Per-application scale overrides
 catacomb msg scale --app-id firefox 1.5
+catacomb msg scale --app-id anki 1.25
 
 # Spawn Shit
 swaybg -i ~/pictures/wallpapers/skeleton.jpg --mode center &
