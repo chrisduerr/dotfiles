610fe10b347d981de26a311c91704aee09fc1bd6
Christian Duerr <contact@christianduerr.com>
2024-01-28 13:17:22 +0100

    Update torrent cleanup script
    This patch adjusts the torrent script to allow for multiple different
    input directories.

--- files/bin/cleanup_torrents.sh
+++ files/bin/cleanup_torrents.sh
@@ -1,72 +1,82 @@
 #!/bin/bash
 
-# Directory where all the Linux ISOs are stored.
-torrent_dir="/mnt/zslow/torrents"
+# Directories where all the Linux ISOs are stored.
+torrent_dirs=("/mnt/zslow/shows/torrents" "/mnt/zfast/movies/torrents")
 
-# Find all files in /mnt/share/torrents/ and /mnt/share/torrents/*/ that have no
-# hardlinks and are at least 5MB in size.
-unlinked=$(find "$torrent_dir" -maxdepth 2 -type f -links 1 -size +5M)
+for torrent_dir in ${torrent_dirs[*]}; do
+    echo "Deleting torrents in ${torrent_dir}…"
 
-# Files and directories which should be removed.
-nukem=()
+    # Find all files in ${torrent_dir} and ${torrent_dir}/* that have no
+    # hardlinks and are at least 5MB in size.
+    unlinked=$(find "$torrent_dir" -maxdepth 2 -type f -links 1 -size +5M)
 
-# Iterate over removable files.
-while IFS= read -r file; do
-    dir=$(dirname "$file")
-    if [[ "$dir" == "$torrent_dir" ]]; then
-        # Add file if torrent is not a directory with multiple files.
-        nukem+=("$file")
-    else
-        # Check if directory was already added.
-        is_duplicate=0
-        for nuke in "${nukem[@]}"; do
-            if [[ "$nuke" == "$dir" ]]; then
-                is_duplicate=1
-                break
+    # Skip directory if there are no files without hardlinks.
+    if [ -z "${unlinked}" ]; then
+        echo -e "\e[32mNo unused torrents found\e[0m"
+        continue
+    fi
+
+    # Files and directories which should be removed.
+    nukem=()
+
+    # Iterate over removable files.
+    while IFS= read -r file; do
+        dir=$(dirname "$file")
+        if [[ "$dir" == "$torrent_dir" ]]; then
+            # Add file if torrent is not a directory with multiple files.
+            nukem+=("$file")
+        else
+            # Check if directory was already added.
+            is_duplicate=0
+            for nuke in "${nukem[@]}"; do
+                if [[ "$nuke" == "$dir" ]]; then
+                    is_duplicate=1
+                    break
+                fi
+            done
+
+            # Skip checks for duplicate directories.
+            if [[ $is_duplicate == 1 ]]; then
+                continue
             fi
-        done
 
-        # Skip checks for duplicate directories.
-        if [[ $is_duplicate == 1 ]]; then
-            continue
+            # Only add directory if no other hardlinked files are in it.
+            linked=$(find "$dir" -maxdepth 1 -type f -links +1 -size +5M)
+            if [[ -z "$linked" ]]; then
+                nukem+=("$dir")
+            fi
         fi
+    done <<< "$unlinked"
 
-        # Only add directory if no other hardlinked files are in it.
-        linked=$(find "$dir" -maxdepth 1 -type f -links +1 -size +5M)
-        if [[ -z "$linked" ]]; then
-            nukem+=("$dir")
-        fi
+    # Skip cleanup if there's nothing to cleanup.
+    if [[ "${#nukem[@]}" == 0 ]]; then
+        echo -e "\e[32mNo unused torrents found\e[0m"
+        continue
     fi
-done <<< "$unlinked"
 
-# Skip cleanup if there's nothing to cleanup.
-if [[ "${#nukem[@]}" == 0 ]]; then
-    echo -e "\e[32mNo unused torrents found\e[0m"
-    exit 0
-fi
-
-# List all torrents which will be deleted.
-echo -e "\e[31mTorrents to be deleted:\e[0m"
-for nuke in "${nukem[@]}"; do
-    shortened=${nuke#"${torrent_dir}/"}
-    if [[ -d "$nuke" ]]; then
-        echo -e "  \e[34m$shortened\e[0m"
-    else
-        echo -e "  \e[32m$shortened\e[0m"
-    fi
-done
-
-# Prompt for confirmation.
-printf "\n\e[31mDelete all unused torrents? [y/N] \e[0m"
-read -n 1 -r
-echo
-
-# Nukeit.
-if [[ $REPLY =~ ^[Yy]$ ]]; then
+    # List all torrents which will be deleted.
+    echo -e "\e[31mTorrents to be deleted:\e[0m"
     for nuke in "${nukem[@]}"; do
-        sudo rm -rf "$nuke"
+        shortened=${nuke#"${torrent_dir}/"}
+        if [[ -d "$nuke" ]]; then
+            echo -e "  \e[34m$shortened\e[0m"
+        else
+            echo -e "  \e[32m$shortened\e[0m"
+        fi
     done
-fi
 
-# Bye. :)
-echo -e "\e[32mSuccessfully deleted all unused torrents.\e[0m"
+    # Prompt for confirmation.
+    printf "\n\e[31mDelete all unused torrents? [y/N] \e[0m"
+    read -n 1 -r
+    echo
+
+    # Nukeit.
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        for nuke in "${nukem[@]}"; do
+            sudo rm -rf "$nuke"
+        done
+    fi
+
+    # Bye. :)
+    echo -e "\e[32mSuccessfully deleted all unused torrents.\e[0m"
+done
