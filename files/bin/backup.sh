ad883c44f2af8e405fb84b1f8ec22574b4a80393
Christian Duerr <contact@christianduerr.com>
2025-01-14 03:00:45 +0100

    Update backup script exclusions

--- files/bin/backup.sh
+++ files/bin/backup.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-exclude="--exclude /programming/**/target \
-         --exclude /programming/**/build \
+exclude="--exclude /programming/*/*/target \
+         --exclude /programming/*/*/build \
          --exclude /programming/massive \
          --exclude /downloads \
+         --exclude /music \
          --exclude /.cache \
+         --exclude /.debug \
          --exclude /.config/nvim/plugins \
          --exclude /.rustup \
          --exclude /.cargo \
+         --exclude /.local/share/Steam/steamapps/compatdata/*/pfx/drive_c/Program* \
+         --exclude /.local/share/Steam/steamapps/shadercache \
          --exclude /.local/share/Steam/steamapps/common \
          --exclude alarm-sysroot"
 
